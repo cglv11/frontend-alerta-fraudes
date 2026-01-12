a6fc06e4d69bb9fc787a4c77bf1f6077cdc4b7d9
Camilo <cglv11@gmail.com>
2026-01-12 13:37:33 -0500

    feat: environmet

---
+++ src/app/core/config/environment.ts
@@ -0,0 +1,6 @@
+// For technical interviews, we hardcode values
+// In production, you would use Angular's environment.ts
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+};
