5e6cdeb77f004cb1273f42c2aac046f5c72abb3b
Camilo <cglv11@gmail.com>
2026-01-12 13:36:35 -0500

    feat: app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CreateTransactionComponent } from './features/transactions/presentation/pages/create-transaction/create-transaction.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/transactions/create',
+    pathMatch: 'full',
+  },
+  {
+    path: 'transactions/create',
+    component: CreateTransactionComponent,
+  },
+];
