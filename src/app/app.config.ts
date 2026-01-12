743df46fe0d007d2b77742f2e45405f014b304db
Camilo <cglv11@gmail.com>
2026-01-12 13:36:18 -0500

    feat: app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app.routes';
+
+// Import repositories
+import { TransactionRepository } from './features/transactions/domain/repositories/transaction.repository';
+import { TransactionApiRepository } from './features/transactions/infrastructure/repositories/transaction-api.repository';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    // Zoneless mode,
+    provideZonelessChangeDetection(),
+
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+
+    // Dependency Injection: Interface → Implementation
+    {
+      provide: TransactionRepository,
+      useClass: TransactionApiRepository,
+    },
+  ],
 };
