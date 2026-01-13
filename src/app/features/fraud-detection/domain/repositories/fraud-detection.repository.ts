9fcb205dacb601570e91687725b57e19cc52df94
Camilo <cglv11@gmail.com>
2026-01-12 21:18:07 -0500

    feat: fraud detection repository domain get users

--- src/app/features/fraud-detection/domain/repositories/fraud-detection.repository.ts
+++ src/app/features/fraud-detection/domain/repositories/fraud-detection.repository.ts
@@ -1,7 +1,11 @@
 import { Observable } from 'rxjs';
 import { UserRiskProfileModel } from '../models/user-risk-profile.model';
+import { UserModel } from '../models/user.model';
 
 // Abstract class to enable Angular Dependency Injection
 export abstract class FraudDetectionRepository {
   abstract getUserRiskProfile(userId: string): Observable<UserRiskProfileModel>;
+
+  // New method
+  abstract getUsers(): Observable<UserModel[]>;
 }
